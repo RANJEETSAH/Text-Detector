dc7cedef6e2bbfbe17dddb7c81c0833803eb0006
Ranjeet <ranjeet.sah14@gmail.com>
2022-08-23 12:15:51 +0530

    update

--- Main.py
+++ Main.py
@@ -9,26 +9,26 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import os
-def foundasin(filename):
+def foundtext(filename):
     f = open(filename, "r")
     filecontent = f.read()
-    if f"{asin}" in filecontent.lower():
+    if f"{text}" in filecontent.lower():
         return True
     else:
         return False
 if __name__ == "__main__":
-    asin=input("Enter ASIN  = ")
+    text=input("Enter any word  = ")
     dir_content = os.listdir()
     #print(dir_content)
-    nASIN= 0
+    ntext= 0
     for item in dir_content:
-        if item.endswith('xlsx'):
-            print(f"Detecting ASIN in {item}")
-            flag = foundasin(item)
+        if item.endswith("txt"):
+            print(f"Detecting {text} in {item}")
+            flag = foundtext(item)
             if (flag):
-                print(f"*******ASIN found in {item}**********")
+                print(f"*******{text} found in {item}**********")
                 nBinod +=1
         else:
-            print(f"ASIN not found in {item}")
-    print("*********ASIN Detector Summary********")
-    print(f"{nASIN} files found with {asin} into them")
+            print(f"{text} not found in {item}")
+    print("*********Text Detector Summary********")
+    print(f"{ntext} files found with {text} into them")
